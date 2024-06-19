38825d1191082d48ea93af33aec82fce5db08e19
William Zhao <williezhao123@gmail.com>
2024-06-18 20:41:47 -0700

    Drew a triangle

--- MyFirstEngine/Sources/main.cpp
+++ MyFirstEngine/Sources/main.cpp
@@ -9,8 +9,62 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main(int argc, char * argv[]) {
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
 
+const GLchar* fragSource = R"glsl(
+    #version 150 core
+
+    out vec4 outColor;
+
+    void main(){
+        outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+)glsl";
+
+GLuint loadShaders() {
+    //Create and Load a Vertex Shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    //Compile
+    glCompileShader(vertexShader);
+
+    //Create and Load Frag Shader
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &fragSource, NULL);
+    glCompileShader(fragShader);
+
+    //Vertext Shader Compilation Debugging
+    GLint vertexStatus, fragStatus;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexStatus);
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragStatus);
+    char buffer[512];
+    glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+    if (vertexStatus && fragStatus) {
+        printf("Shaders compiled successfully!");
+    }
+
+    //Create shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+
+    //Specify what buffer Frag Shader output is written
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    return shaderProgram;
+}
+
+int main(int argc, char * argv[]) {
     // Load GLFW and Create a Window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -31,18 +85,58 @@ int main(int argc, char * argv[]) {
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+
+
+    //Vertex Array Object VAO: Stores attributes and VBO links
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+
+    //Creates a Vertex Buffer Object (VBO)
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    float vertices[] = {
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+        -0.5f, -0.5f
+    };
+
+    //Activates the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    //Copy Vertex Data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint shaderProgram = loadShaders();
+
+    //Get reference to postion from vertex shader
+    GLint posAttribute = glGetAttribLocation(shaderProgram, "position");
+    //Enable the attribute
+    glEnableVertexAttribArray(posAttribute);
+    //Specify how data is retrieved from VBO
+    glVertexAttribPointer(posAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    
+
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, true);
 
+
+
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+       
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
-    }   glfwTerminate();
+    }
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
